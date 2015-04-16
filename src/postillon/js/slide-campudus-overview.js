744b798fa855c37918e0ed304d478637785df35e
Joern Bernhardt <jb@campudus.com>
2015-04-16 08:14:46 +0200

    Happy accident at random texter :)
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/postillon/js/slide-campudus-overview.js
+++ src/postillon/js/slide-campudus-overview.js
@@ -2,31 +2,22 @@ var adj = [ 'attraktiven', 'beliebten', 'bescheidenen', 'coolen', 'epischen', 'e
 var sub = [ 'Designer', 'Freunde', 'Gorillas', 'Helden', 'Jungs', 'Kids', 'Kellerkinder', 'Knacker', 'Kollegen', 'Krieger', 'Männer', 'Nerds', 'Ninjas', 'Opas', 'Roboter', 'Tiere', 'Typen', 'Programmierer', 'Zauberer', 'Zebras' ];
 
 var $slide = document.getElementById('slide-campudus-overview');
-var adjs = [];
-var subs = [];
-adjs[0] = $slide.querySelector('.adj.text-1');
-adjs[1] = $slide.querySelector('.adj.text-2');
-subs[0] = $slide.querySelector('.sub.text-1');
-subs[1] = $slide.querySelector('.sub.text-2');
+var texts = [];
+texts[0] = $slide.querySelector('.text-1');
+texts[1] = $slide.querySelector('.text-2');
 
 var i = 0;
 var running = false;
 var timer;
 
 function next() {
-  adjs[i].innerHTML = adj[Math.floor(Math.random() * adj.length)];
-  subs[i].innerHTML = sub[Math.floor(Math.random() * sub.length)];
-  adjs[i].style.translateY = '-50';
-  adjs[i].classList.add('active');
-  adjs[i].classList.remove('inactive');
-  subs[i].classList.add('active');
-  subs[i].style.translateY = '50';
-  subs[i].classList.remove('inactive');
+  texts[i].innerHTML = adj[Math.floor(Math.random() * adj.length)] + ' ' + sub[Math.floor(Math.random() * sub.length)];
+  texts[i].style.translateY = '-50';
+  texts[i].classList.add('active');
+  texts[i].classList.remove('inactive');
   i = (i + 1) % 2;
-  adjs[i].classList.add('inactive');
-  adjs[i].classList.remove('active');
-  subs[i].classList.add('inactive');
-  subs[i].classList.remove('active');
+  texts[i].classList.add('inactive');
+  texts[i].classList.remove('active');
 
   if (running) {
     timer = setTimeout(next, 1000);
