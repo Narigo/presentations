6d423eb82339a019b665c2d2b850d4da2bc8cffc
Joern Bernhardt <jb@campudus.com>
2015-05-07 08:41:41 +0200

    Add 3-minute timer
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/campudus-3/js/app.js
+++ src/campudus-3/js/app.js
@@ -19,6 +19,30 @@ Reveal.addEventListener('slidechanged', function (event) {
   }
 });
 
+var timeGes = 60 * 3 * 1000;
+var lastRun = (+new Date());
+function timer() {
+  var diff = (+new Date()) - lastRun;
+  setTimeout(function () {
+    lastRun = (+new Date());
+    timeGes = timeGes - 1000;
+    document.getElementById('timer').innerHTML = nf(Math.max(0, timeGes / 1000));
+    timer();
+  }, 1000 - diff);
+}
+timer();
+
+function nf(num) {
+  var h = padZero(Math.floor(num / 60 / 60));
+  var m = padZero(Math.floor(num / 60));
+  var s = padZero(num % 60);
+  return h + ':' + m + ':' + s;
+}
+
+function padZero(x) {
+  return (x < 10) ? '0' + x : '' + x;
+}
+
 Reveal.initialize({
   controls : true,
   progress : true,
